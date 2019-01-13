cd40f2733ac8ce71227f418b708af6c8f2de5947
Manabu Kikuchi <16217397+kerobot@users.noreply.github.com>
2019-01-14 01:51:10 +0900

    学習処理修正

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # img_model_gen
+
+To Categorical And Save Model by Keras
+
+画像ファイルをもとに学習データとテストデータのラベル付けおよびモデルの保存を行います。
+
+## 環境
+
+* Windows 10 x64 1809
+* Python 3.6.5 x64
+* Power Shell 6 x64
+* Visual Studio Code x64
+* Git for Windows x64
+
+## 構築
+
+プロジェクトを clone してディレクトリに移動します。
+
+```powershell
+> git clone https://github.com/kerobot/img_model_gen.git img_model_gen
+> cd img_model_gen
+```
+
+プロジェクトのための仮想環境を作成して有効化します。
+
+```powershell
+> python -m venv venv
+> .\venv\Scripts\activate.ps1
+```
+
+念のため、仮想環境の pip をアップグレードします。
+
+```powershell
+> python -m pip install --upgrade pip
+```
+
+依存するパッケージをインストールします。
+
+```powershell
+> pip install -r requirements.txt
+```
+
+## 実行
+
+face_scratch_imageディレクトリに学習データ、test_imageディレクトリにテストデータとなる画像ファイルを配置して実行します。
+
+> モデルファイルの出力
+
+```powershell
+> python .\img_model_gen.py
+```
--- img_model_gen.py
+++ img_model_gen.py
@@ -1,17 +1,20 @@
-# 画像と正解ラベルをリストにする
-import random
-from keras.utils.np_utils import to_categorical
+import sys
 import os
+import random
+import pathlib
+import shutil
+import glob
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.utils.np_utils import to_categorical
 
 print("学習データのラベル付け開始")
 
 img_file_name_list=os.listdir("./face_scratch_image/")
 print(len(img_file_name_list))
 
-for i in range(len(img_file_name_list)):
+for i in range(0,len(img_file_name_list)):
     n=os.path.join("./face_scratch_image",img_file_name_list[i])
     img = cv2.imread(n)
     if isinstance(img,type(None)) == True:
@@ -21,7 +24,7 @@ print(len(img_file_name_list))
 
 X_train=[]
 y_train=[]
-for j in range(0,len(img_file_name_list)-1):
+for j in range(0,len(img_file_name_list)):
     n=os.path.join("./face_scratch_image/",img_file_name_list[j])
     img = cv2.imread(n)
     b,g,r = cv2.split(img)
@@ -32,12 +35,13 @@ for j in range(0,len(img_file_name_list)-1):
 X_train=np.array(X_train)
 
 print("学習データのラベル付け終了")
+
 print("テストデータのラベル付け開始")
 
 img_file_name_list=os.listdir("./test_image/")
 print(len(img_file_name_list))
 
-for i in range(len(img_file_name_list)):
+for i in range(0,len(img_file_name_list)):
     n=os.path.join("./test_image",img_file_name_list[i])
     img = cv2.imread(n)
     if isinstance(img,type(None)) == True:
@@ -59,11 +63,22 @@ X_test=np.array(X_test)
 
 print("テストデータのラベル付け終了")
 
-from keras.layers import Activation, Conv2D, Dense, Flatten, MaxPooling2D
-from keras.models import Sequential, load_model
+print("X_train.shape:", X_train.shape)
+print("y_train.shape:", y_train.shape)
+print("X_test.shape:", X_test.shape)
+print("y_test.shape:", y_test.shape)
 
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
+from keras.layers import Activation, Conv2D, Dense, Flatten, MaxPooling2D, Dropout
+from keras.models import Sequential, load_model
+from keras.callbacks import TensorBoard
+
+# 特徴量の正規化
+# X_train = X_train / 255.
+# X_test = X_test / 255.
+
+# クラスラベルの1-hotベクトル化
+y_train = to_categorical(y_train, 2)
+y_test = to_categorical(y_test, 2)
 
 # plt.imshow(X_train[0])
 # plt.show()
@@ -71,28 +86,48 @@ y_test = to_categorical(y_test)
 
 # モデルの定義
 model = Sequential()
-model.add(Conv2D(input_shape=(64, 64, 3), filters=32,kernel_size=(2, 2), strides=(1, 1), padding="same"))
+
+# フィルタを用いてストライド1で特徴マップを計算
+# input_shape   入力データのサイズ 64 x 64 x RGB
+# filters       出力チャンネル数
+# kernel_size   フィルタ(=カーネル)のサイズ数．3x3とか5x5とか奇数正方にすることが一般的
+# strides       ストライドの幅(フィルタを動かすピクセル数)
+# padding       データの端の取り扱い方(入力データの周囲を0で埋める(ゼロパディング)ときは'same',ゼロパディングしないときは'valid')
+# activation    活性化関数
+model.add(Conv2D(input_shape=(64, 64, 3), filters=32,kernel_size=(3, 3), strides=(1, 1), padding="same", activation='relu'))
+# 2x2の4つの領域に分割して各2x2の行列の最大値をとる
+# データが縮小されることで計算コストが軽減される+各領域内の位置の違いを無視するためモデルが小さな位置変化に対して頑健となる
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(filters=32, kernel_size=(2, 2), strides=(1, 1), padding="same"))
+
+model.add(Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), padding="same", activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(filters=32, kernel_size=(2, 2), strides=(1, 1), padding="same"))
+
+model.add(Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), padding="same", activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# ドロップアウト層
+model.add(Dropout(0.01))
+
+# 全結合層(プーリング層の出力は4次元テンソルであるため2次元テンソルに展開)
 model.add(Flatten())
+
 model.add(Dense(256))
 model.add(Activation('sigmoid'))
+
 model.add(Dense(128))
 model.add(Activation('sigmoid'))
-model.add(Dense(5))
+
+model.add(Dense(2))
 model.add(Activation('softmax'))
 
 # コンパイル
 model.compile(optimizer='sgd', loss='categorical_crossentropy',metrics=['accuracy'])
 
 # 学習
-model.fit(X_train, y_train, batch_size=32, epochs=50)
+# model.fit(X_train, y_train, batch_size=32, epochs=60)
 
 # グラフ用
-history = model.fit(X_train, y_train, batch_size=32, epochs=100, verbose=1, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train, batch_size=32, epochs=60, verbose=1, validation_data=(X_test, y_test))
 
 # 汎化制度の評価・表示
 score = model.evaluate(X_test, y_test, batch_size=32, verbose=0)
@@ -107,4 +142,4 @@ plt.legend(loc="best")
 plt.show()
 
 # モデルを保存
-model.save("my_model.h5")
+model.save("model.h5")
